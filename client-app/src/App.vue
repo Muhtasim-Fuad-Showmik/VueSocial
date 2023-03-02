cfe15ba07a033ae458bc5e06c3fa11213573b35d
Muhtasim-Fuad-Showmik <muhtasimfuadshowmik@gmail.com>
2023-03-02 16:18:37 +0600

    Added an empty Vue3 frontend client to the application

---
+++ client-app/src/App.vue
@@ -0,0 +1,22 @@
+<!-- eslint-disable @typescript-eslint/no-unused-vars -->
+<template>
+  <div>
+    <RouterView />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, RouterView } from 'vue'
+
+export default defineComponent({
+  setup () {
+    
+
+    return {}
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
