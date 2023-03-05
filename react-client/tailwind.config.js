6d7f353f876520ee26beb599c4bd69f710cb8cac
Muhtasim-Fuad-Showmik <muhtasimfuadshowmik@gmail.com>
2023-03-05 15:53:37 +0600

    Updated tailwind configuration to include the index.html page within the public folder

--- react-client/tailwind.config.js
+++ react-client/tailwind.config.js
@@ -2,10 +2,14 @@
 module.exports = {
   content: [
     "./index.html",
+    "./public/index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      Poppins: ["Poppins, sans-serif"]
+    }
   },
   plugins: [],
 }
