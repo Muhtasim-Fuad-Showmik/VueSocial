f61dc3204430615ced296f2941dc25c6f3262f97
Muhtasim-Fuad-Showmik <muhtasimfuadshowmik@gmail.com>
2023-03-05 12:52:15 +0600

    Accessed API data from the react client and displayed a list of them

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    interface Activity {
+        id: string;
+        title: string;
+        description: string;
+        date: string;
+        category: string;
+        city: string;
+        venue: string;
+    }
+    const initialActivities: Activity[] = [];
+    const [activities, setActivities] = useState(initialActivities);
+    const api = "http://localhost:5000";
+
+    useEffect(() => {
+        axios.get(`${api}/api/activities`).then((response) => {
+            setActivities(response.data);
+        });
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                {activities.length && (
+                    <ul>
+                        {activities.map((activity: Activity) => (
+                            <li key={activity.id}>{activity.title}</li>
+                        ))}
+                    </ul>
+                )}
+            </header>
+        </div>
+    );
 }
 
 export default App;
