78303cc02a84d1a2438e3a1bb37a9762e23b73ab
Muhtasim-Fuad-Showmik <muhtasimfuadshowmik@gmail.com>
2023-03-05 12:51:11 +0600

    Accessed API data from the vue client and displayed a list of them

--- client-app/src/views/HomeView.vue
+++ client-app/src/views/HomeView.vue
@@ -1,7 +1,32 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import axios from 'axios';
+
+interface Activity {
+	id: string,
+	title: string,
+	description: string,
+	date: string,
+	category: string,
+	city: string,
+	venue: string
+}
+
+const initialActivities: Activity[] = [];
+const activites = ref(initialActivities);
+const getActivities = async () => {
+	axios.get("http://localhost:5000/api/activities").then(response => {
+		activites.value = response.data;
+	});
+};
+getActivities();
 </script>
 
 <template>
-  <main>
-  </main>
+	<main>
+		<h1>Activites</h1>
+		<ul>
+			<li v-for="activity in activites" :key="activity.id">{{ activity.title }}</li>
+		</ul>
+	</main>
 </template>
