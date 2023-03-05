e6fa1b6b039854797bdab4d489d2a2a3152f1067
Muhtasim-Fuad-Showmik <muhtasimfuadshowmik@gmail.com>
2023-03-05 17:29:24 +0600

    Styled layout template and current list content for react client

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -22,23 +22,26 @@ function App() {
     }, []);
 
     return (
-        <div>
-            <nav className="w-100 h-16 max-h-16 backdrop-blur-sm sticky top-0 z-50">
+        <div className="h-screen">
+            <nav className="w-full h-16 max-h-16 px-12 backdrop-blur-sm sticky top-0 z-50 shadow-md">
                 <div className="w-16 h-16 flex items-center">
-                    <img src="./Logo.png" alt="logo" className="w-100 h-100" />
+                    <img src="./Logo.png" alt="logo" className="w-full h-full" />
                     <span className="text-3xl font-bold text-primaryDark">React</span>
                     <span className="text-3xl">Social</span>
                 </div>
             </nav>
-            <div className="">
-                <h1>Activites</h1>
-                {activities.length && (
-                    <ul>
-                        {activities.map((activity: Activity) => (
-                            <li key={activity.id}>{activity.title}</li>
-                        ))}
-                    </ul>
-                )}
+            <div className="bg-gradient-to-r from-primary to-orangeSoda pl-10 pb-10 h-[calc(100vh-64px)]">
+                <div className="bg-white h-full p-10 rounded">
+                    {/* Main Content */}
+                    <h1 className="font-bold text-2xl mb-4">Activites</h1>
+                    {activities.length && (
+                        <ul>
+                            {activities.map((activity: Activity) => (
+                                <li className="mb-2" key={activity.id}>{activity.title}</li>
+                            ))}
+                        </ul>
+                    )}
+                </div>
             </div>
         </div>
     );
