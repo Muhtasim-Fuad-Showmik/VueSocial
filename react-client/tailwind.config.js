d4ef3cf4a701da9be24207e0844bb55c53aa4c76
Muhtasim-Fuad-Showmik <muhtasimfuadshowmik@gmail.com>
2023-03-05 16:05:42 +0600

    Added custom color palletes to the react client

--- react-client/tailwind.config.js
+++ react-client/tailwind.config.js
@@ -6,7 +6,15 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "antiFlashWhite": "#edf2f4",
+        "primary": "#ef233c",
+        "primaryDark": "#d90429",
+        "secondary": "#8d99ae",
+        "secondaryDark": "#2b2d42",
+      }
+    },
     fontFamily: {
       Poppins: ["Poppins, sans-serif"]
     }
