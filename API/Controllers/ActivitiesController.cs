dcbc2fc67baf2e1da51c21c6f913e74c538b31e2
Muhtasim-Fuad-Showmik <muhtasimfuadshowmik@gmail.com>
2023-03-06 17:11:44 +0600

    Implemented class for fetching individual activity

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -32,7 +32,7 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Domain.Activity>> GetActivity(Guid id)
         {
-            return Ok();
+            return await Mediator.Send(new Details.Query{Id = id});
         }
     }
 }
