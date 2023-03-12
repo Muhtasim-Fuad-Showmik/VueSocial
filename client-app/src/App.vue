e6ceaacde11cfc401e4ecb84c0d154f76ad725be
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2023-03-12 15:53:45 +0600

    Added a nav bar to the vue client

--- client-app/src/App.vue
+++ client-app/src/App.vue
@@ -1,13 +1,7 @@
 <!-- eslint-disable @typescript-eslint/no-unused-vars -->
 <template>
 	<div class="h-screen">
-		<nav class="w-full h-16 max-h-16 px-12 backdrop-blur-sm sticky top-0 z-50 shadow-md">
-			<div class="w-16 h-16 flex items-center">
-				<img src="/Logo.png" alt="logo" class="w-full h-full" />
-				<span class="text-3xl font-bold text-primaryDark">Vue</span>
-				<span class="text-3xl">Social</span>
-			</div>
-		</nav>
+		<NavBar />
 		<div class="bg-gradient-to-r from-primary to-orangeSoda pl-10 pb-10 h-[calc(100vh-64px)]">
 			<div class="bg-white h-full p-10 rounded">
 				<RouterView />
@@ -18,12 +12,13 @@
 
 <script lang="ts">
 import { defineComponent, RouterView } from 'vue'
+import NavBar from './components/layout/NavBar.vue';
 
 export default defineComponent({
-	setup() {
-
-		return {} 
-	}
+    setup() {
+        return {};
+    },
+    components: { NavBar }
 })
 </script>
 
---
+++ client-app/src/components/layout/NavBar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<nav class="w-full h-16 max-h-16 flex items-center px-12 backdrop-blur-sm sticky top-0 z-50 shadow-md">
+        <div class="h-16 mr-4 flex items-center">
+            <img src="/Logo.png" alt="logo" class="w-full h-full" />
+            <span class="text-3xl font-bold text-primaryDark">Vue</span>
+            <span class="text-3xl">Social</span>
+        </div>
+        <div class="ml-4">
+                <a href="/">Activities</a>
+            </div>
+            <div class="ml-4">
+                <a href="/">Notifications</a>
+            </div>
+    </nav>
+</template>
